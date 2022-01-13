f5f8b24fc29c77d9bc54ae4fb97af47455da6d84
Servasch <62065008+servasch@users.noreply.github.com>
2022-01-13 16:17:24 +0100

    Update LiechtensteinSARIMA01.py

--- LiechtensteinSARIMA01.py
+++ LiechtensteinSARIMA01.py
@@ -3,24 +3,35 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-import statsmodels.api as sm
-from statsmodels.tsa.stattools import adfuller
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-from sklearn.metrics import mean_squared_error
-from math import sqrt
-import warnings
-warnings.filterwarnings('ignore')
+#import seaborn as sns
+#import statsmodels.api as sm
+#from statsmodels.tsa.stattools import adfuller
+#from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+#from sklearn.metrics import mean_squared_error
+#from math import sqrt
+#import warnings
+#warnings.filterwarnings('ignore')
 #%matplotlib inline
 
 # Reading and transforming the file
+print("hello1")
 epd = pd.read_csv('C:/Users/c8451269/PycharmProjects/CovidDataset/LiechtensteinSARIMA01/EPD.csv')
-epd.head(5)
-#activeCases = epd['confirmed'] - epd['deaths'] - epd['recovered']
+
+print(epd.head(5))
+print("hello")
+
+epd['date'] = pd.to_datetime(epd['Unnamed: 0'],infer_datetime_format=True) #convert from string to datetime
+print(epd.head(5))
+epd['activeCases'] = epd['confirmed'] - epd['deaths'] - epd['recovered']
+print(epd.head(5))
+print(epd['date'].head(5))
+
+epd1 = epd['date'].max()-epd['date'].min()
+print(epd1)
 
 
+filt=(epd['date']>=pd.to_datetime('2021-01-01'))
+print(epd.loc[filt])
 
-#I'm going to consider the temperature just from 1900 until the end of 2012
-#rio = rio.loc['1900':'2013-01-01']
-#rio = rio.asfreq('M', method='bfill')
-#rio.head()
+epd.set_index('date', inplace=True) #consider date as index, not the numbers anymore (for the rows)
+print(epd['2021-01':'2021-02']) #now that the dates are considered index we can do this and write it in this easy way
